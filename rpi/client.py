acdc6701dca081380a27607371e207b2c7d5f995
Dillon Amadeo <divestrato@dillonamadeo.local>
2014-09-26 10:58:51 +0800

    updated python client for testing

--- rpi/client.py
+++ rpi/client.py
@@ -9,11 +9,11 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.connect(('192.168.10.10', 8888))
 
 while 1:
-    print "input status: "
+    print "input type: "
     data_type = stdin.readline().strip()
     print "input data: "
     data_data = stdin.readline().strip()
-    data = data = {"type": data_type, "data": data_data}
+    data = {"type": data_type, "data": data_data}
     print ">>>>>>> SENDING >>>>>>>"
-    json_data = json.dumps(data)
-    sock.send(json_data)
+    json_string = json.dumps(data)
+    sock.send(json_string)
