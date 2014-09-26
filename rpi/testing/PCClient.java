13aca017c626220ca46137d1d0389266231ac342
Dillon Amadeo <divestrato@dillonamadeo.local>
2014-09-26 11:00:02 +0800

    added barebone java class for PC testing

---
+++ rpi/testing/PCClient.java
@@ -0,0 +1,17 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+
+class PCClient {
+	public static void main(String[] args) {
+		PCClient client = new PCClient("localhost", 8888)
+		try {
+
+		} catch () {
+
+		} catch () {
+			
+		}
+	}
+}
