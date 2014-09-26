4caf592c25969f5f5fa25a4d7ebec63ffbc6067e
Richard Lesmana <Richard Lesmana@172.20.135.48>
2014-09-26 09:34:58 +0800

    Changes to Dijkstra

--- algorithm/src/mdpAlgorithm/Dijkstra.java
+++ algorithm/src/mdpAlgorithm/Dijkstra.java
@@ -30,26 +30,25 @@ class Edge {
 public class Dijkstra { // Based on Dijkstra algorithm	
 	
     //public static void computePaths(Vertex source, Robot rob)
-    public static void computePaths(Vertex source) {
-        source.minDistance = 0.;
+    public static void computePaths(Vertex startPoint) {
+        startPoint.minDistance = 0.;
         PriorityQueue<Vertex> vertexQueue = new PriorityQueue<Vertex>();
-      	vertexQueue.add(source);
+      	vertexQueue.add(startPoint);
 
 	while (!vertexQueue.isEmpty()) {
 	    Vertex u = vertexQueue.poll();
 
             // Visit each edge exiting u
-            for (Edge e : u.adjacencies)
-            {
-                Vertex v = e.target;
-                double weight = e.weight;
-                double distanceThroughU = u.minDistance + weight;
-		if (distanceThroughU < v.minDistance) {
-		    vertexQueue.remove(v);
-		    v.minDistance = distanceThroughU ;
-		    v.previous = u;
-		    vertexQueue.add(v);
-		}
+            for (Edge e : u.adjacencies) { 	// For-each loop to the edges that are adjacent to the current vertex
+                Vertex v = e.target; 								// Sets the next target as vertex v
+                double weight = e.weight; 							// Inserts the edge's weight
+                double distanceThroughU = u.minDistance + weight; 	// Total dist from the startPoint is added to weight
+				if (distanceThroughU < v.minDistance) { 			// If the distanceThruU is there (not infinity)
+				    vertexQueue.remove(v);
+				    v.minDistance = distanceThroughU ; 				// The minDistance is added to the vertex v
+				    v.previous = u; 								// V's parent is u
+				    vertexQueue.add(v); 							// Add v to the priorityQ
+				}
             }
         }
     }
@@ -63,8 +62,33 @@ public class Dijkstra { // Based on Dijkstra algorithm
     }
 
     public static void main(String[] args) {
-    	
-    	//Need to use the printed MapDescriptor3Testing here
+    	    	
+    	/*	Map Descriptor 3: 0 unexplored, 1 explored&empty, 2 explored&obstacle
+    	 *  
+    	 *  MD3 for testing:
+    	 * 
+    	 	111111112000000
+			111111112000000
+			111111112000000
+			111111112222000
+			111111111111200
+			222111111111200
+			002111111111222
+			002111111111111
+			002111111111111
+			002111111111111
+			002111111111111
+			002111111111111
+			002111111111111
+			002111111111111
+			002111111111111
+			000221112222111
+			000021112002111
+			000021112002111
+			000021112002111
+			000021112002111
+
+    	 */
 
 	    Vertex v0 = new Vertex("Redvile");
 		Vertex v1 = new Vertex("Blueville");
@@ -84,6 +108,21 @@ public class Dijkstra { // Based on Dijkstra algorithm
 		                             new Edge(v4, 2) };
 		v4.adjacencies = new Edge[]{ new Edge(v1, 7),
 	                               new Edge(v3, 2) };
+		
+		/*
+		 * 
+		 Vertex currentGrid = new Vertex("Current Point");
+		 
+		 
+		 currentGrid.adjacencies = new Edge[] { new Edge(north,1),
+		 										new Edge(west, 1),
+		 										new Edge(south, 1),
+		 										new Edge(east, 1),
+		 									  };
+		 
+		 Vertex[] vertices { currentGrid };
+		 * 
+		 */
 	    
 	    	
 		Vertex[] vertices = { v0, v1, v2, v3, v4 };
