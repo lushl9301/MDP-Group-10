08fa17743c88ca332b46f44003fa39908054023e
Dillon Amadeo <divestrato@dillonamadeo.local>
2014-09-26 22:03:39 +0800

    flesh out the java client for testing

--- rpi/testing/PCClient.java
+++ rpi/testing/PCClient.java
@@ -4,14 +4,65 @@ import java.io.OutputStreamWriter;
 import java.io.BufferedWriter;
 
 class PCClient {
+
+	private String hostname;
+	private int port;
+	Socket socketClient;
+
+	public PCClient(String hostname, int port){
+		this.hostname = hostname;
+		this.port = port;
+	}
+
+	public void connect() throws UnknownHostException, IOException {
+		System.out.println("Attempting connection %");
+		socketClient = new Socket(hostname, port);
+		System.out.println("Connection established");
+	}
+
+	public void sendJSON(String type, String data) {
+		Map map = new HashMap();
+		map.put("type", type);
+		map.put("data", data);
+		String jsonString = JSONValue.toJSONString(map);
+
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(self.socketClient.getOutputStream()));
+		writer.write(jsonString);
+		writer.flush();
+		writer.close();
+	}
+
+	public void readInput() {
+		BufferedWriter br = new BufferedWriter(new InputStreamReader(System.in));
+		String type = "";
+		String data = "";
+
+		while (true) {
+			try {
+				System.out.print("Input type: ");
+				type = br.readLine();
+				System.out.print("Input data: ");
+				data = br.readLine();
+
+				self.sendJSON(type, data);
+			} catch (IOException e) {
+				System.out.println("IO error");		
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		PCClient client = new PCClient("localhost", 8888)
 		try {
-
-		} catch () {
-
-		} catch () {
+			client.connect();
+			// if successful, try to get data
 			
+			client.readInput();
+
+		} catch (UnknownHostException e) {
+			System.err.println("Unknown Host");
+		} catch (IOException e) {
+			System.err.println("Cannot establish connection. "+e.getMessage());
 		}
 	}
 }
