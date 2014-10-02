78296a359f8e62e25f604cd11586ed5761cacc98
Dillon Amadeo <divestrato@dillonamadeo.local>
2014-10-02 11:06:58 +0800

    updated client

--- rpi/client.py
+++ rpi/client.py
@@ -4,6 +4,23 @@ import json
 from sys import stdin
 
 
+def modeReading():
+    while 1:
+        print "input x: "
+        data_x = stdin.readline().strip()
+        if data_x == "-1":
+            print "=======[ EXIT ]======="
+            break
+        print "input y: "
+        data_y = stdin.readline().strip()
+        print "input direction: "
+        data_direction = stdin.readline().strip()
+        data_data = {"x": data_x, "y": data_y, "direction": data_direction}
+        data = {"type": "reading", "data": data_data}
+        print ">>>>>>> SENDING >>>>>>>"
+        json_string = json.dumps(data)
+        sock.send(json_string + "\n")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -12,9 +29,12 @@ sock.connect((WIFI_IP, WIFI_PORT))
 while 1:
     print "input type: "
     data_type = stdin.readline().strip()
-    print "input data: "
-    data_data = stdin.readline().strip()
-    data = {"type": data_type, "data": data_data}
-    print ">>>>>>> SENDING >>>>>>>"
-    json_string = json.dumps(data)
-    sock.send(json_string + "\n")
+    if data_type == "reading":
+        modeReading()
+    else:
+        print "input data: "
+        data_data = stdin.readline().strip()
+        data = {"type": data_type, "data": data_data}
+        print ">>>>>>> SENDING >>>>>>>"
+        json_string = json.dumps(data)
+        sock.send(json_string + "\n")
